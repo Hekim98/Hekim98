a659da3b0e3417ff03b40a63394217db38ed0916
Hekim <84737366+Hekim98@users.noreply.github.com>
2022-05-26 20:46:31 +0300

    Create ödev7.sql

---
+++ ödev7.sql
@@ -0,0 +1,24 @@
+--Aşağıdaki sorgu senaryolarını dvdrental örnek veri tabanı üzerinden gerçekleştiriniz.
+
+--film tablosunda bulunan filmleri rating değerlerine göre gruplayınız.
+
+select rating, count(*) from film
+group by rating
+
+--film tablosunda bulunan filmleri replacement_cost sütununa göre grupladığımızda film sayısı 50 den
+
+select replacement_cost, count(*)from film
+group by replacement_cost
+having count(*) > 50
+
+--customer tablosunda bulunan store_id değerlerine karşılık gelen müşteri sayılarını nelerdir?
+
+select store_id, count(*)from customer
+group by store_id
+
+-- 4. city tablosunda bulunan şehir verilerini country_id sütununa göre gruplandırdıktan sonra en fazla
+-- şehir sayısı barındıran country_id bilgisini ve şehir sayısını paylaşınız.
+SELECT country_id, COUNT(*) FROM city
+GROUP BY country_id
+ORDER BY COUNT(*) DESC
+LIMIT 1;
